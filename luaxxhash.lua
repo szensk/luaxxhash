251da1e2237415018e78f42c055acd1440947b43
Jon Havron <jmhavron@gmail.com>
2016-09-12 22:29:40 -0500

    clarify data aliases

--- luaxxhash.lua
+++ luaxxhash.lua
@@ -1,16 +1,15 @@
 local ffi = require('ffi')
 local bit = require('bit')
+
 local rotl, xor, band, shl, shr = bit.rol, bit.bxor, bit.band, bit.lshift, bit.rshift
-local u32type = ffi.typeof("uint32_t")
+local uint32_t = ffi.typeof("uint32_t")
 
 -- Prime constants
-local P1 = (2654435761)
-local P2 = (2246822519)
-local P3 = (3266489917)
-local P4 = (668265263)
-local P5 = (374761393)
-local U1 = u32type(P1)
-local U2 = u32type(P2)
+local P1 = uint32_t(0x9E3779B1)
+local P2 = uint32_t(0x85EBCA77)
+local P3 = (0xC2B2AE3D)
+local P4 = (0x27D4EB2F)
+local P5 = (0x165667B1)
 
 -- multiplication with modulo2 semantics
 -- see https://github.com/luapower/murmurhash3
@@ -19,23 +18,22 @@ local function mmul(a, b)
 	return tonumber(ffi.cast(type, ffi.cast(type, a) * ffi.cast(type, b)))
 end
 
-local function xxhash32(data, seed, len)
-	seed = seed or 0
-	len = len or #data
-	local p = ffi.cast("const uint8_t*", data)
-	data = ffi.cast('const uint32_t*', data)
+local function xxhash32(data, len, seed)
+	seed, len = seed or 0, len or #data
+	local i,n = 0, 0 -- byte and word index
+	local bytes = ffi.cast( 'const uint8_t*', data)
+	local words = ffi.cast('const uint32_t*', data)
+
 	local h32 = 0
-	local i = 0 -- byte index
-	local n = 0 -- 4 byte index
 	if len >= 16 then
 		local limit = len - 16
 		local v = ffi.new("uint32_t[4]")
-		v[0], v[1] = seed + U1 + U2, seed + U2
-		v[2], v[3] = seed, seed - U1
+		v[0], v[1] = seed + P1 + P2, seed + P2
+		v[2], v[3] = seed, seed - P1
 		while i <= limit do 
 			for j=0, 3 do
-				v[j] = v[j] + data[n] * U2
-				v[j] = rotl(v[j], 13); v[j] = v[j] * U1
+				v[j] = v[j] + words[n] * P2
+				v[j] = rotl(v[j], 13); v[j] = v[j] * P1
 				i = i + 4; n = n + 1
 			end
 		end
@@ -47,13 +45,13 @@ local function xxhash32(data, seed, len)
 
 	local limit = len - 4
 	while i <= limit do
-		h32 = (h32 + mmul(data[n], P3))
+		h32 = (h32 + mmul(words[n], P3))
 		h32 = mmul(rotl(h32, 17), P4)
 		i = i + 4; n = n + 1
 	end
 
 	while i < len do
-		h32 = h32 + mmul(p[i], P5)
+		h32 = h32 + mmul(bytes[i], P5)
 		h32 = mmul(rotl(h32, 11), P1)
 		i = i + 1
 	end
@@ -62,8 +60,7 @@ local function xxhash32(data, seed, len)
 	h32 = mmul(h32, P2)
 	h32 = xor(h32, shr(h32, 13))
 	h32 = mmul(h32, P3)
-	h32 = xor(h32, shr(h32, 16))
-	return h32
+	return xor(h32, shr(h32, 16))
 end
 
 return xxhash32
